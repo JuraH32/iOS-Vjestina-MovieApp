75f0b6aa29de936d0a1391abd6b34be2ae8dc29c
jhostic <jura.hostic@gmail.com>
2023-03-23 08:22:54 +0100

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by endava-bootcamp on 23.03.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
